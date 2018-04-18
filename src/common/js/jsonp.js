6af4b8024b945fdee9ed7cbacafb123c357cfd81
Yida <442570303@qq.com>
2018-04-18 23:45:13 +0800

    feat: first commit

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,21 @@
+import originJsonp from 'jsonp'
+
+export default function jsonp(url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+
+  return new Promise((resolve, reject) => {
+    originJsonp(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+export function param(data) {
+  return Object.entries(data)
+    .map(([key, value]) => key + '=' + encodeURIComponent(value))
+    .join('&')
+}
