17fc10ea360a07e628b653427d16c5610d1ae32f
Yida <442570303@qq.com>
2018-05-03 21:43:20 +0800

    chore: 修改代码规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,33 @@
 // http://eslint.org/docs/user-guide/configuring
 module.exports = {
-    root: true,
-    parser: 'babel-eslint',
-    parserOptions: {
-        sourceType: 'module'
-    },
-    env: {
-        browser: true,
-    },
-    // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-    extends: 'standard',
-    // required to lint *.vue files
-    plugins: [
-        'html'
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: ['html'],
+  // add your custom rules here
+  rules: {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'eol-last': 0,
+    'space-before-function-paren': [
+      'error',
+      { anonymous: 'always', named: 'never', asyncArrow: 'always' }
     ],
-    // add your custom rules here
-    'rules': {
-        // allow paren-less arrow functions
-        'arrow-parens': 0,
-        // allow async-await
-        'generator-star-spacing': 0,
-        // allow debugger during development
-        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-        'eol-last': 0,
-        'space-before-function-paren': 0,
-        // 允许空行结尾有空格
-        'no-trailing-spaces': ['warn', { 'skipBlankLines': true }],
-        // 强制语句分号结尾
-        'semi': ['error', 'always']
-    }
+    // 允许空行结尾有空格
+    'no-trailing-spaces': ['warn', { skipBlankLines: true }],
+    // 强制语句分号结尾
+    semi: ['error', 'always']
+  }
 }
